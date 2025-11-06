a21b032c0d5a4a78ca471393b761c099462b4665
isabellaaddas <isabellaaddas@gmail.com>
2025-11-06 14:42:50 -0500

    server up

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -5,7 +5,7 @@ import MenuItemText from "./MenuItemText";
 const MenuItem = (props) => {
     return (
         <div className="menu-item columns">
-            <div className="menu-item-img one"><Link to="#"><img src={props.image}/></Link></div>
+            <div className="menu-item-img one"><Link to="#"><img src={"https://server-t-and-z-deli.onrender.com/images/" + props.image}/></Link></div>
         
             <MenuItemText name={props.name} price={props.price} description={props.description}/>
         </div>
--- src/components/MenuSection.jsx
+++ src/components/MenuSection.jsx
@@ -1,14 +1,154 @@
 import "./../css/MenuSection.css";
 import MenuItem from "./MenuItem";
+import {useState, useEffect} from "react";
+import axios from "axios";
 
 const MenuSection = (props) => {
-    return (
-        <div id={props.ident} className="menu-section">
-            <h1>{props.category}</h1>
+    const [orders, setOrders] = useState([]);
 
-            <MenuItem name="NAPLES STYLE" price="11.99" image="https://isabellaaddas.github.io/csce242/projects/json/images/neapolitan-style.jpg" description='A classic panuozzo ("pizza sandwich") full of the familiar flavors of prosciutto and parmesan.'/>
-        </div>
-    );
+    useEffect(() => {
+        const loadOrders = async() => {
+            const response = await axios.get("https://server-t-and-z-deli.onrender.com/api/orders");
+            setOrders(response.data);
+        };
+
+        loadOrders();
+    }, []);
+
+    if (props.ident == "sandwiches") {
+        return (
+            <div id={props.ident} className="menu-section">
+                <h1>{props.category}</h1>
+
+                {orders.filter((order) => {
+                    return order.category == "sandwich";
+                }).map((order) => {
+                    return (
+                        <MenuItem   name={order.name}
+                                    price={order.price}
+                                    image={order.img}
+                                    description={order.description}
+                                    key={order._id}
+                                    id={order._id} />
+                    );
+                })}
+            </div>
+        );
+    } else if (props.ident == "soups") {
+        return (
+            <div id={props.ident} className="menu-section">
+                <h1>{props.category}</h1>
+
+                {orders.filter((order) => {
+                    return order.category == "soup";
+                }).map((order) => {
+                    return (
+                        <MenuItem   name={order.name}
+                                    price={order.price}
+                                    image={order.img}
+                                    description={order.description}
+                                    key={order._id}
+                                    id={order._id} />
+                    );
+                })}
+            </div>
+        );
+    } else if (props.ident == "cold-cuts") {
+        return (
+            <div id={props.ident} className="menu-section">
+                <h1>{props.category}</h1>
+
+                {orders.filter((order) => {
+                    return order.category == "cold-cut";
+                }).map((order) => {
+                    return (
+                        <MenuItem   name={order.name}
+                                    price={order.price}
+                                    image={order.img}
+                                    description={order.description}
+                                    key={order._id}
+                                    id={order._id} />
+                    );
+                })}
+            </div>
+        );
+    } else if (props.ident == "jarred-goods") {
+        return (
+            <div id={props.ident} className="menu-section">
+                <h1>{props.category}</h1>
+
+                {orders.filter((order) => {
+                    return order.category == "jarred";
+                }).map((order) => {
+                    return (
+                        <MenuItem   name={order.name}
+                                    price={order.price}
+                                    image={order.img}
+                                    description={order.description}
+                                    key={order._id}
+                                    id={order._id} />
+                    );
+                })}
+            </div>
+        );
+    } else if (props.ident == "cheese") {
+        return (
+            <div id={props.ident} className="menu-section">
+                <h1>{props.category}</h1>
+
+                {orders.filter((order) => {
+                    return order.category == "cheese";
+                }).map((order) => {
+                    return (
+                        <MenuItem   name={order.name}
+                                    price={order.price}
+                                    image={order.img}
+                                    description={order.description}
+                                    key={order._id}
+                                    id={order._id} />
+                    );
+                })}
+            </div>
+        );
+    } else if (props.ident == "extras") {
+        return (
+            <div id={props.ident} className="menu-section">
+                <h1>{props.category}</h1>
+
+                {orders.filter((order) => {
+                    return order.category == "extra";
+                }).map((order) => {
+                    return (
+                        <MenuItem   name={order.name}
+                                    price={order.price}
+                                    image={order.img}
+                                    description={order.description}
+                                    key={order._id}
+                                    id={order._id} />
+                    );
+                })}
+            </div>
+        );
+    } else if (props.ident == "desserts") {
+        return (
+            <div id={props.ident} className="menu-section">
+                <h1>{props.category}</h1>
+
+                {orders.filter((order) => {
+                    return order.category == "dessert";
+                }).map((order) => {
+                    return (
+                        <MenuItem   name={order.name}
+                                    price={order.price}
+                                    image={order.img}
+                                    description={order.description}
+                                    key={order._id}
+                                    id={order._id} />
+                    );
+                })}
+            </div>
+        );
+    }
 };
 
 export default MenuSection;
