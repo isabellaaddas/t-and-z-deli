43054c5bf3203ead4bd89e814ad1b7260b4733c5
isabellaaddas <isabellaaddas@gmail.com>
2025-12-04 12:17:19 -0500

    final touches

--- src/components/MenuItemButtons.jsx
+++ src/components/MenuItemButtons.jsx
@@ -1,16 +1,28 @@
 import "./../css/MenuItemButtons.css";
+import {useState} from "react";
 
 const MenuItemButtons = () => {
+    const [counter, setCounter] = useState(0);
 
     const handleClick = (event) => {
         event.preventDefault();
-    }
+    };
+
+    const plusOne = (e) => {
+        e.preventDefault();
+        counter >= 99 ? setCounter(99):setCounter(counter + 1);
+    };
+
+    const minusOne = (e) => {
+        e.preventDefault();
+        counter <= 0 ? setCounter(0):setCounter(counter - 1);
+    };
 
     return (
         <div className="counter-buttons one" onClick={handleClick}>
-            <button className="minus">-</button>
-            <span className="counter">1</span>
-            <button className="plus">+</button>
+            <button className="minus" onClick={minusOne}>-</button>
+            <span className="counter">{counter}</span>
+            <button className="plus" onClick={plusOne}>+</button>
         </div>
     );
 };
